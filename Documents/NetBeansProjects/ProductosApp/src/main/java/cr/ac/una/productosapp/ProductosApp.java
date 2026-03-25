91e579c344713cfeee9f86232500d7e0b1575ef4
Freyman Guzmán <freymanguzman8@gmail.com>
2026-03-25 13:39:46 -0600

    create project

---
+++ Documents/NetBeansProjects/ProductosApp/src/main/java/cr/ac/una/productosapp/ProductosApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cr.ac.una.productosapp;
+
+/**
+ *
+ * @author Freyman
+ */
+public class ProductosApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
